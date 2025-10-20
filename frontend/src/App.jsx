12a37bb597374d3c74f5efba1103849450e3d681
Quentin TUFFERY <quentin.tuffery@gmail.com>
2025-10-20 14:31:05 +0200

    Add initial files for various node modules and configurations

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import SearchBar from "./components/SearchBar";
+import './App.css'
+import data from './assets/sample_data.json'
+
+function App() {
+
+
+  return (
+    <>
+    <header>
+      <h1>
+        EcoTrain
+      </h1>
+    </header>
+    <main>
+    </main>
+    </>
+  );
+}
+
+export default App
---
+++ frontend/src/components/SearchBar.jsx
@@ -0,0 +1,60 @@
+function SearchBar() {
+  return (
+    <form>
+      <label>
+        Date du trajet:
+        <input type="date" name="date" required />
+      </label>
+      <br />
+      <label>
+        Gare de départ:
+        <input type="text" name="departureStation" placeholder="Ex: Paris" required />
+      </label>
+      <br />
+      <label>
+        Gare d'arrivée:
+        <input type="text" name="arrivalStation" placeholder="Ex: Troyes" required />
+      </label>
+      <br />
+      <label>
+        Heure de départ:
+        <input type="time" name="minTime" step="3600" required />
+      </label>
+      <br />
+      <fieldset>
+        <legend>Voyageur 1</legend>
+        <div>
+          <label>
+            Catégorie:
+            <select name="category" required>
+              <option value="bebe">Bébé (0-3 ans)</option>
+              <option value="enfant">Enfant (4-11 ans)</option>
+              <option value="jeune">Jeune (12-25 ans)</option>
+              <option value="adulte">Adulte (26-59 ans)</option>
+              <option value="senior">Senior (60 ans ou plus)</option>
+            </select>
+          </label>
+          <br />
+          <label>
+            Âge:
+            <input type="number" name="age" min="0" required />
+          </label>
+          <br />
+          <label>
+            Carte de réduction:
+            <select name="category">
+              <option value="carteavantage">Carte avantage</option>
+              <option value="carteavantagejeune">Carte avantage jeune</option>
+              <option value="fluo">Carte Fluo</option>
+              <option value="fluojeune">Carte fluo jeune</option>
+            </select>
+          </label>
+        </div>
+      </fieldset>
+      <br />
+      <button type="submit">Rechercher un trajet</button>
+    </form>
+  );
+}
+
+export default SearchBar;
