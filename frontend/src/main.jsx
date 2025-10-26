ceb57db3e050073ecd0d01a64e9f619421b33a34
Quentin TUFFERY <quentin.tuffery@gmail.com>
2025-10-26 18:27:05 +0100

    REFACTOR : Updated Pico CSS theme to orange variant

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import '@picocss/pico/css/pico.css'
+import '@picocss/pico/css/pico.orange.css' 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
