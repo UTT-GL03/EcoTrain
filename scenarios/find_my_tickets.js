d5543ffffc5d151e846aa3db1426412586ff7363
Quentin TUFFERY <quentin.tuffery@gmail.com>
2025-12-15 15:07:03 +0100

    fix: greenframe can't access to the ticket as it's stored online

--- scenarios/find_my_tickets.js
+++ scenarios/find_my_tickets.js
@@ -19,13 +19,6 @@ const visit = async (page) => {
     await page.waitForTimeout(10000);
     await page.scrollToEnd();
     await page.waitForNetworkIdle();
-    await page.addMilestone('Open a ticket');
-    await page.goto('https://nbviewer.org/github/UTT-GL03/EcoTrain/blob/billets/frontend/public/placeholder_ticket.pdf', {
-        waitUntil: 'networkidle',
-    });
-    await page.waitForTimeout(10000);
-    await page.scrollToEnd();
-    await page.waitForNetworkIdle();
     await page.waitForTimeout(7000);
   };
   
